0fc9f07f936449aecce88871d33604d4546af89a
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-10-25 13:13:53 -0700

    Make it work

--- src/main.js
+++ src/main.js
@@ -1,18 +1,83 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-var $ = require('jQuery')
+// var $ = require('jQuery')
+// console.log($)
+
+
+
+
+
+
+
+
+
+
+    var Lib = {
+        ajax: {
+            xhr: function() {
+                var instance = new XMLHttpRequest()
+                return instance
+            },
+            // code continues
+            getJSON: function(options, callback) {
+                            var xhttp = this.xhr()
+                            options.url = options.url || location.href
+                            options.data = options.data || null
+                            callback = callback ||
+                            function() {}
+                            options.type = options.type || 'json'
+                            var url = options.url
+
+                            if (options.type == 'jsonp') { // JSONP
+                                window.jsonCallback = callback // Now our callback method is globally visible
+                                var urlZ = url.replace('callback=?', 'callback=jsonCallback')
+                                var script = document.createElement('script')
+                                script.src = urlZ
+                                document.body.appendChild(script)
+                            }
+
+
+                            xhttp.open('GET', options.url, true)
+                            xhttp.send(options.data)
+                            xhttp.onreadystatechange = function() {
+                                if (xhttp.status == 200 && xhttp.readyState == 4) {
+                                    callback(xhttp.responseText)
+                                }
+                            }
+            }
+        }
+    }
+
+    window.Lib = Lib
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function success (json) {
-  render(json.weather[0].id)
+  var weatherData = JSON.parse(json)
+  render(weatherData.weather[0].id)
 }
+
+
 
 var site = 'http://api.openweathermap.org/data/2.5/weather?'
 var id = '&APPID=c40e96f8fee7a7c8ba301e8a9e9f0347'
 var geoLocation
 
-$.getJSON('http://freegeoip.net/json/', function(json) {
-geoLocation = 'lat=' + json.latitude + '&lon=' + json.longitude
-  $.getJSON(site + geoLocation + id, success)
+Lib.ajax.getJSON({url: 'http://freegeoip.net/json/'}, function(jsonString) {
+  var locationInfo = JSON.parse(jsonString)
+  geoLocation = 'lat=' + locationInfo.latitude + '&lon=' + locationInfo.longitude
+  Lib.ajax.getJSON({url: site + geoLocation + id}, success)
 })
 
 function render(weather) {
